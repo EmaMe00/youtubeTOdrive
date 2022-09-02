dccaef3dd6e05fdf78c26286f8cc2be698c5dbf5
EmaMe00 <100135146+EmaMe00@users.noreply.github.com>
2022-09-02 12:23:21 +0200

    Update downloadYTPlaylist.py

--- downloadYTPlaylist.py
+++ downloadYTPlaylist.py
@@ -9,7 +9,6 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-#variable for control error
 controlError = 0
 
 #Autentication on Google
@@ -18,20 +17,18 @@ gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
 #Playlist on youtube
-p = Playlist('https://www.youtube.com/playlist?list=PLTtrab7aFnMXiwBc9DbhGAe0-pp252wh0')
+p = Playlist('PLAYLIST URL')
 
 #Folder path to save video
 SAVE_PATH = ("YOUR PATH" + p.title)
 os.mkdir(SAVE_PATH)
 
-#File for error
-with open("ERROR_"+p.title, 'a') as f:
+with open(SAVE_PATH+"/ERROR_"+p.title, 'a') as f:
     f.write("Downloading Error: \n")
     f.close()
 
 print(f'\nDownloading playlist: {p.title}. Total element: {len(p.videos)}\n')
 
-#I've different try except for download becouse if resolution don't exist for the video, this system try with a lower resolution
 counter = 0
 for video in p.videos:
     try:
@@ -51,7 +48,7 @@ for video in p.videos:
                 controlError = 1
                 print(e)
                 print("Error to download " + video.title + "\n")
-                with open("ERROR_"+p.title(), 'a') as f:
+                with open(SAVE_PATH+"/ERROR_"+p.title(), 'a') as f:
                     f.write(video.title + '\n')
                     f.close()
 
@@ -59,39 +56,47 @@ print("Playlist downloaded !\n")
 
 # Get the list of all files and directories
 dir_list = os.listdir(SAVE_PATH)
+#print("Files and directories in '", path, "' :")
+# prints all files
+#print(dir_list)
+
 
 file_path = SAVE_PATH + "/"
-folderID = "YOUR DRIVE ID FOLDER"
+folderID = "YOUR DRIVE FOLDER ID"
 
-#File for error message
-with open("ERROR_"+p.title, 'a') as f:
+with open(SAVE_PATH+"/ERROR_"+p.title, 'a') as f:
     f.write("\nUploading Error: \n")
     f.close()
 
 counter = 0
 for filename in dir_list:
     try:
-        counter = counter + 1
-        print(f"Uploading: {filename}. Number {counter}/{len(p.videos)}")
-        file1 = drive.CreateFile({'title': filename , 'parents': [{'id': folderID}]})
-        file1.SetContentFile(file_path+filename)
-        file1.Upload()
-        print(f'Uploaded: {filename}\n')
+        if(filename != ("ERROR_"+p.title)):
+            counter = counter + 1
+            print(f"Uploading: {filename}. Number {counter}/{len(p.videos)}")
+            file1 = drive.CreateFile({'title': filename , 'parents': [{'id': folderID}]})
+            file1.SetContentFile(file_path+filename)
+            file1.Upload()
+            print(f'Uploaded: {filename}\n')
+            os.remove(file_path + filename)
+
     except Exception as e:
         controlError = 1
         print(e)
         print("\nError to upload " + filename + "\n")
-        with open("ERROR_"+p.title, 'a') as f:
+        with open(SAVE_PATH+"/ERROR_"+p.title, 'a') as f:
             f.write(filename + '\n')
             f.close()
 
+
+dir_list = os.listdir(SAVE_PATH)
 if (controlError == 0):
     print("Removing file from pc ...\n")
     for filename in dir_list:
         os.remove(file_path + filename)
 
     os.rmdir(SAVE_PATH)
-    os.remove("/Users/emanuelemele/Desktop/Altro/downloadYTPlaylistPY/" +"ERROR_"+p.title)
+    print("File removed!\n")
 
-print("File removed!\n")
 print("Finish !")
+
